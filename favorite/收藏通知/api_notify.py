cbb04347c976792413027cf4600b5e6b9f599890
MisakaF0406 <76677904+MisakaF0406@users.noreply.github.com>
2022-10-17 10:11:16 +0800

    Update api_notify.py

--- favorite/收藏通知/api_notify.py
+++ favorite/收藏通知/api_notify.py
@@ -4,9 +4,12 @@ from urllib.parse import unquote
 import json
 import telegram
 import pymysql
+import requests
 
 api = flask.Flask(__name__) 
 bot = telegram.Bot(token='****')
+embyurl = '****'
+embyapikey = '****'
 #连接数据库
 connect = pymysql.connect(host='****', 
                           user='****',
@@ -117,6 +120,48 @@ def add_favourite(chatid,itemid):
         print(e)
     else:
         return 1
+
+def server_add(embyid,itemid,chatid):
+    headers = {
+        'accept': 'application/json',
+    }
+
+    params = {
+        'api_key': embyapikey,
+    }
+
+    response = requests.post(embyurl + 'emby/Users/'+embyid+'/FavoriteItems/'+itemid, params=params, headers=headers)
+
+    try: 
+        sql = "UPDATE favorite set locked=1 where chatid="+str(chatid)
+        print(sql)
+        db.execute(sql)
+        connect.commit()
+    except Exception as e:
+        print(e)
+    else:
+        return 1
+
+def server_cancel(embyid,itemid,chatid):
+    headers = {
+        'accept': 'application/json',
+    }
+
+    params = {
+        'api_key': embyapikey,
+    }
+
+    response = requests.delete(embyurl + 'emby/Users/'+embyid+'/FavoriteItems/'+itemid, params=params, headers=headers)
+
+    try: 
+        sql = "UPDATE favorite set locked=1 where chatid="+str(chatid)
+        print(sql)
+        db.execute(sql)
+        connect.commit()
+    except Exception as e:
+        print(e)
+    else:
+        return 1
             
 
 @api.route('/update',methods=['post'])
@@ -135,27 +180,49 @@ def update():
     if(item_type == 'Series'):
         if(check_exist == 0):
             try: 
-                sql = "insert into favorite (chatid,emby_userid,fav) values ("+str(chatid)+","+'"'+str(emby_id)+'"'+","+'"'+str(item_id)+'"'+")"
+                sql = "insert into favorite (chatid,emby_userid,fav,locked) values ("+str(chatid)+","+'"'+str(emby_id)+'"'+","+'"'+str(item_id)+'"'+",0)"
                 print(sql)
                 db.execute(sql)
                 connect.commit()
             except Exception as e:
                 print(e)
             else:
-                message = "[EmbyVip]"+item_name+"：收藏成功"
+                server_add(emby_id,item_id,chatid)
+                message = "[Emby公益服]"+item_name+"：收藏成功"
                 bot.send_message(chat_id=chatid, text=message)
         elif(check_exist == 1):
             check_like = check_allready_like(chatid,item_id)
-            if(check_like == 0):
-                back = add_favourite(chatid,item_id)
-                if(back == 1):
-                    message = "[EmbyVip]"+item_name+"：收藏成功"
-                    bot.send_message(chat_id=chatid, text=message)
-            elif(check_like == 1):
-                back = cancel_favourite(chatid,item_id)
-                if(back == 1):
-                    message = "[EmbyVip]"+item_name+"：取消收藏成功"
-                    bot.send_message(chat_id=chatid, text=message)
+            try:
+                create_sqli = "SELECT * FROM favorite WHERE chatid =  "+'"'+ str(chatid)+'"'
+                db.execute(create_sqli)
+                result = db.fetchall()
+            except Exception as e:
+                print(e)
+            else:
+                lock = result[0][3]
+            if(lock == '0'):
+                if(check_like == 0):
+                    back = add_favourite(chatid,item_id)
+                    server_add(emby_id,item_id,chatid)
+                    if(back == 1):
+                        message = "[Emby公益服]"+item_name+"：收藏成功"
+                        bot.send_message(chat_id=chatid, text=message)
+                elif(check_like == 1):
+                    back = cancel_favourite(chatid,item_id)
+                    server_cancel(emby_id,item_id,chatid)
+                    if(back == 1):
+                        message = "[Emby公益服]"+item_name+"：取消收藏成功"
+                        bot.send_message(chat_id=chatid, text=message)
+            elif(lock == '1'):
+                try: 
+                    sql = "UPDATE favorite set locked=0 where chatid="+str(chatid)
+                    print(sql)
+                    db.execute(sql)
+                    connect.commit()
+                except Exception as e:
+                    print(e)
+                else:
+                    print("skip!")
     return('200')
   
 if __name__ == '__main__':
